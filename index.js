fa26a6aff348ca6aa86fca01bae95194250a134e
Miguel Flores <miguelflores.dev@gmail.com>
2023-05-09 15:56:55 -0700

    codificando botón copiar

--- index.js
+++ index.js
@@ -39,23 +39,16 @@ function desencriptar(){
         .replace(/ober/gi, "o")
         .replace(/fat/gi, "u");
 
-        if (texto.length !=0) {
+    if (texto.length !=0) {
             document.getElementById("texto").value=textoCifrado;
             tituloMensaje.textContent="Texto desencriptado con exito";
             parrafo.textContent="";
             buscando.src ="./img/desencriptado.jpg";
-        }else{
+    }else{
             buscando.src = "./img/buscando.png";  
             tituloMensaje.textContent="Ningun mensaje fue encontrado";
             parrafo.textContent="Ingresa el texto que deseas encriptar o desencriptar";      
             swal ("Ooops!","Debes ingresar un texto","warning");        
-         }
+    }
     
-}
-
-function copiar(){              
-        texto.select();
-        texto.setSelectionRange(0, 99999);
-        document.CommandId("copy");
-       
-}
+}     
